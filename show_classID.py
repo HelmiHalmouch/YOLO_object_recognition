c7601e7aaf1500d30819ac54391e41b3a351f8c3
HelmiHalmouch <helmirealmadrid@gmail.com>
2021-04-04 19:24:59 +0200

    update code style

--- show_classID.py
+++ show_classID.py
@@ -1,10 +1,8 @@
 # load the label of classe
 ClasseFile = 'datasets.names'
 classes = None
-
 with open(ClasseFile, 'r') as f :
 	#classes = len(f.readlines())
 	classes = f.read().rstrip('\n').split('\n')
 	print(classes)
-	print('#-----------#')
-	print('The bumber of classes is :{}'.format(len(classes)))
+	print('The number of classes is :{}'.format(len(classes)))
